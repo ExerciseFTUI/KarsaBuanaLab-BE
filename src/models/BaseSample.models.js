b159761d952736fc52d04c71124a1e59c0b6cb85
IdleGoat <rafie.amandio@ui.ac.id>
2023-12-05 21:48:29 +0700

    Update new model and minor fix

--- src/models/BaseSample.models.js
+++ src/models/BaseSample.models.js
@@ -1,15 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { regulationSchema } = require("./Regulation.models");
 
 const baseSampleSchema = new mongoose.Schema({
-    sample_name: { type: String, required: true, unique: true },
-    file_id: { type: String, required: true },
-    param: [{ type: String, required: false }],
-    regulation: { type: String, required: false },
+  sample_name: { type: String, required: true, unique: true },
+  file_id: { type: String, required: true },
+  file_safety_id: { type: String, required: true },
+  param: [{ type: String, required: false }],
+  regulation: [regulationSchema],
 });
 
 const BaseSample = mongoose.model("BaseSample", baseSampleSchema);
 
 module.exports = {
-    baseSampleSchema,
-    BaseSample
-}
+  baseSampleSchema,
+  BaseSample,
+};
+/*
+Air LImbah
+Param : PH, Kebeningan, kekentalan
+File : 
+Regulation : 
+-> reg_name : PP 2014
+    default_param : PH,Kebeningan
+
+sample
+
+*/
--- src/models/Regulation.models.js
+++ src/models/Regulation.models.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const regulationSchema = new mongoose.Schema({
   regulation_name: { type: String, required: true },
-  param: [{ type: String, required: true }],
+  default_param: [{ type: String, required: true }],
 });
 
 const Regulation = mongoose.model("Regulation", regulationSchema);
