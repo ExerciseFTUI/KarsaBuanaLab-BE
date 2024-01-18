f470225b5c393f96722cf2a5944676589db09034
MorpKnight <christoffelsihombing@gmail.com>
2024-01-18 16:49:48 +0700

    perbaikin dashboard

--- src/services/Marketing.services.js
+++ src/services/Marketing.services.js
@@ -1,13 +1,15 @@
 const { Project } = require("../models/Project.models");
 const { BaseSample } = require("../models/BaseSample.models");
 
+
+
 exports.dashboard = async function (req, res) {
     const approvedOffer = await getApprovedOffer();
     const totalProject = await getTotalProjectPerYear();
     const totalClient = await getTotalClientPerYear();
-    const projectCancelled = await projectCancelledPerMonth();
-    const projectRunning = await projectRunningPerMonth();
-    const projectFinished = await projectFinishedPerMonth();
+    const projectCancelled = await projectStatusPerMonth("CANCELLED");
+    const projectRunning = await projectStatusPerMonth("RUNNING");
+    const projectFinished = await projectStatusPerMonth("FINISHED");
     const offerPerMonth = await getOfferPerMonth();
 
     const result = {
@@ -20,7 +22,7 @@ exports.dashboard = async function (req, res) {
         offerPerMonth: offerPerMonth
     }
 
-    return {message: "Success", result};
+    return { message: "Success", result };
 }
 
 exports.getSample = async function (req, res) {
@@ -44,93 +46,77 @@ exports.getProjectByID = async function (params) {
     return { message: "Success", result };
 }
 
-async function getApprovedOffer(){
-    const result = await Project.find({status: "RUNNING"} || {status: "FINISHED"}).exec();
+async function getApprovedOffer() {
+    const result = await Project.find({ status: "RUNNING" } || { status: "FINISHED" }).exec();
     return result;
 }
 
-async function getTotalProjectPerYear(){
-    const result = await Project.find({status: "RUNNING"} || {status: "FINISHED"}).exec();
+async function getTotalProjectPerYear() {
+    const result = await Project.find({ status: "RUNNING" } || { status: "FINISHED" }).exec();
     const projectList = result.map(project => project.project_name);
     const projectCount = projectList.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
     const totalProject = Object.keys(projectCount).length;
     return totalProject;
 }
 
-async function getTotalClientPerYear(){
-    const result = await Project.find({status: "RUNNING"} || {status: "FINISHED"}).exec();
+async function getTotalClientPerYear() {
+    const result = await Project.find({ status: "RUNNING" } || { status: "FINISHED" }).exec();
     const projectList = result.map(project => project.client_name);
     const projectCount = projectList.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
     const totalProject = Object.keys(projectCount).length;
     return totalProject;
 }
 
-
-// TODO : Butuh revisi karena models baru dan database masih menggunakan model lama
-async function projectCancelledPerMonth(){
-    const result = await Project.find({status: "CANCELLED"}).exec();
-    const projectList = result.map(project => project.project_name);
-    const projectPerMonth = {};
-    const currentYear = new Date().getFullYear();
-    projectList.forEach(project => {
-        if (new Date(project.created_at).getFullYear() !== currentYear) return;
-        const month = new Date(project.created_at).getMonth();
-        if (projectPerMonth[month] === undefined) {
-            projectPerMonth[month] = 1;
-        } else {
-            projectPerMonth[month] += 1;
-        }
+async function getOfferPerMonth() {
+    const resultRunning  = await Project.find({ status: "RUNNING" }).exec();
+    const resultFinished = await Project.find({ status: "FINISHED" }).exec();
+    const projectPerMonth = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0,
+        11: 0
+    };
+    resultRunning.forEach(project => {
+        if (new Date(project.created_at).getFullYear() !== new Date().getFullYear()) return;
+        projectPerMonth[new Date(project.created_at).getMonth()]++;
     });
+
+    resultFinished.forEach(project => {
+        if (new Date(project.created_at).getFullYear() !== new Date().getFullYear()) return;
+        projectPerMonth[new Date(project.created_at).getMonth()]++;
+    });
+
     return projectPerMonth;
 }
 
-async function projectRunningPerMonth(){
-    const result = await Project.find({status: "RUNNING"}).exec();
-    const projectList = result.map(project => project.project_name);
-    const projectPerMonth = {};
-    const currentYear = new Date().getFullYear();
-    projectList.forEach(project => {
-        if (new Date(project.created_at).getFullYear() !== currentYear) return;
-        const month = new Date(project.created_at).getMonth();
-        if (projectPerMonth[month] === undefined) {
-            projectPerMonth[month] = 1;
-        } else {
-            projectPerMonth[month] += 1;
-        }
-    });
-    return projectPerMonth;
-}
+async function projectStatusPerMonth(status) {
+    const result = await Project.find({ status: status }).exec();
+    const projectPerMonth = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+        10: 0,
+        11: 0
+    };
 
-async function projectFinishedPerMonth(){
-    const result = await Project.find({status: "FINISHED"}).exec();
-    const projectList = result.map(project => project.project_name);
-    const projectPerMonth = {};
-    const currentYear = new Date().getFullYear();
-    projectList.forEach(project => {
-        if (new Date(project.created_at).getFullYear() !== currentYear) return;
-        const month = new Date(project.created_at).getMonth();
-        if (projectPerMonth[month] === undefined) {
-            projectPerMonth[month] = 1;
-        } else {
-            projectPerMonth[month] += 1;
-        }
-    });
-    return projectPerMonth;
-}
-
-async function getOfferPerMonth(){
-    const result = await Project.find({status: "RUNNING"} || {status: "FINISHED"}).exec();
-    const projectList = result.map(project => project.project_name);
-    const projectPerMonth = {};
-    const currentYear = new Date().getFullYear();
-    projectList.forEach(project => {
-        if (new Date(project.created_at).getFullYear() !== currentYear) return;
-        const month = new Date(project.created_at).getMonth();
-        if (projectPerMonth[month] === undefined) {
-            projectPerMonth[month] = 1;
-        } else {
-            projectPerMonth[month] += 1;
-        }
+    result.forEach(project => {
+        if (new Date(project.created_at).getFullYear() !== new Date().getFullYear()) return;
+        projectPerMonth[new Date(project.created_at).getMonth()]++;
     });
     return projectPerMonth;
 }
