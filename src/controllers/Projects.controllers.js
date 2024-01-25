8f362a714a391e35002a8c6ae32c6e3687613a7e
Eriqo90AW <eriqo.arief@ui.ac.id>
2024-01-25 22:13:42 +0700

    create project json

--- src/controllers/Projects.controllers.js
+++ src/controllers/Projects.controllers.js
@@ -62,6 +62,31 @@ exports.createProject = async function (req, res) {
   }
 };
 
+exports.createProjectJSON = async function (req, res) {
+    try {
+      const result = await projectsServices.createProjectJSON(req.body);
+      res.status(200).json(result);
+    } catch (errorObj) {
+      if (errorObj.new_folder_id == null) {
+        res.status(400).json({
+          message: errorObj.message,
+          result: "Failed to create project folder",
+        });
+        return;
+      }
+      const delete_folder = await drivesServices.deleteFile({
+        file_id: errorObj.new_folder_id,
+      });
+      res.status(400).json({
+        message: errorObj.message,
+        result:
+          delete_folder == null
+            ? "Failed to delete the project folder"
+            : "Project folder deleted",
+      });
+    }
+  };
+
 exports.getSample = async function (req, res) {
   try {
     const result = await projectsServices.getSample(req.body);
