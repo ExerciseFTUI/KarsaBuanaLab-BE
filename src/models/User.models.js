27af8a8166ea14a4b2b5d543ddd3b0ed01b43743
MorpKnight <christoffelsihombing@gmail.com>
2024-02-02 20:47:52 +0700

    add createdAt

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -4,11 +4,21 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: {
-      type: String,
-      required: true
+    type: String,
+    required: true,
   },
-  role: { type: String, enum: ["ADMIN", "USER", "SPV"], required: false, default: "USER" },
-  division: { type: String, required: true, enum: ["Marketing", "Sampling", "Lab", "PPLHP"] },
+  role: {
+    type: String,
+    enum: ["ADMIN", "USER", "SPV"],
+    required: false,
+    default: "USER",
+  },
+  division: {
+    type: String,
+    required: false,
+    enum: ["Marketing", "Sampling", "Lab", "PPLHP"],
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
