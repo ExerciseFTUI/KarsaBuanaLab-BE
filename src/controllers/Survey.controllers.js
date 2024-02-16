b7ee0d3881565b26ed06207058e3dbaddc42f8d1
IdleGoat <rafie.amandio@ui.ac.id>
2024-02-16 17:30:42 +0700

    Survey CRUD Finished

---
+++ src/controllers/Survey.controllers.js
@@ -0,0 +1,19 @@
+const surveyServices = require("../services/Survey.services");
+
+exports.createSurvey = async function (req, res) {
+  try {
+    const result = await surveyServices.createSurvey(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.getSurvey = async function (req, res) {
+  try {
+    const result = await surveyServices.getSurvey(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
