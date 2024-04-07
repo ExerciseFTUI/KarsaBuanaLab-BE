994712acd285c95a939f00ee5240c98b03a71395
IdleGoat <rafie.amandio@ui.ac.id>
2024-04-07 15:58:55 +0700

    Update New Inventory

--- src/routes/Inventory.routes.js
+++ src/routes/Inventory.routes.js
@@ -4,6 +4,8 @@ const inventoryControllers = require("../controllers/Inventory.controllers");
 
 router.get("/get-all-inventory", inventoryControllers.getAllInventory);
 router.post("/create-inventory", inventoryControllers.createInventory);
+router.post("/get-inventory-by-id", inventoryControllers.getInventoryItemById);
+router.post("/update-inventory",inventoryControllers.updateInventory);
 
 
 
