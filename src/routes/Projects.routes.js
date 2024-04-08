44612424a5bae8cf78e0047b08a86450b009fc2d
MorpKnight <christoffelsihombing@gmail.com>
2024-04-08 22:11:18 +0700

    add endpoint to get notes for project

--- src/routes/Projects.routes.js
+++ src/routes/Projects.routes.js
@@ -28,5 +28,6 @@ router.get("/get-all-pphlp-detail", projectsControllers.getAllPPLHPDetail);
 router.get("/get-pphlp-detail/:id", projectsControllers.getPPLHPDetail);
 router.post("/lhp-accept/:id", projectsControllers.LHPAccept);
 router.post("/lhp-revision/:id", projectsControllers.LHPRevision);
+router.get("/get-notes/:id", projectsControllers.getNotes);
 
 module.exports = router;
