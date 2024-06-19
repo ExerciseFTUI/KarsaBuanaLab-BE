396cebcc9805749b6f6d37e7f9f1f6166abf7133
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-06-19 20:10:38 +0700

    check client login

--- src/services/Clients.services.js
+++ src/services/Clients.services.js
@@ -17,6 +17,7 @@ exports.login = async function (body) {
       status: project.status,
       current_division: project.current_division,
     };
+    console.log(projectReturn);
     return { message: "Login Successful!", result: projectReturn };
   } else {
     throw new Error("Incorrect Password");
