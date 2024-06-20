c12076d6f0c96c6791e757b059894242311c9036
rafieamandio <rafie.amandio@ui.ac.id>
2024-06-20 08:34:08 +0700

    New PProds

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["ADMIN", "USER", "SPV"],
+    enum: ["ADMIN", "USER", "SPV", "MANAGER TEKNIS"],
     required: false,
     default: "USER",
   },
